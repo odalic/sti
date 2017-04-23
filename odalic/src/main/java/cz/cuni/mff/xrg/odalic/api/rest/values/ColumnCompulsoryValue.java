f4cebe3794a899484d7ce2dc95d5e23158400f89
josef-janousek <pauto@centrum.cz>
2017-04-23 22:11:30 +0200

    new classes for compulsory columns

---
+++ odalic/src/main/java/cz/cuni/mff/xrg/odalic/api/rest/values/ColumnCompulsoryValue.java
@@ -0,0 +1,55 @@
+package cz.cuni.mff.xrg.odalic.api.rest.values;
+
+import java.io.Serializable;
+
+import javax.annotation.Nullable;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.google.common.base.Preconditions;
+
+import cz.cuni.mff.xrg.odalic.feedbacks.ColumnCompulsory;
+import cz.cuni.mff.xrg.odalic.positions.ColumnPosition;
+
+/**
+ * Domain class {@link ColumnCompulsory} adapted for REST API.
+ *
+ * @author Josef Janoušek
+ *
+ */
+@XmlRootElement(name = "columnCompulsory")
+public final class ColumnCompulsoryValue implements Serializable {
+
+  private static final long serialVersionUID = -9087389821835847372L;
+
+  private ColumnPosition position;
+
+  public ColumnCompulsoryValue() {}
+
+  public ColumnCompulsoryValue(final ColumnCompulsory adaptee) {
+    this.position = adaptee.getPosition();
+  }
+
+  /**
+   * @return the position
+   */
+  @XmlElement
+  @Nullable
+  public ColumnPosition getPosition() {
+    return this.position;
+  }
+
+  /**
+   * @param position the position to set
+   */
+  public void setPosition(final ColumnPosition position) {
+    Preconditions.checkNotNull(position);
+
+    this.position = position;
+  }
+
+  @Override
+  public String toString() {
+    return "ColumnCompulsoryValue [position=" + this.position + "]";
+  }
+}
