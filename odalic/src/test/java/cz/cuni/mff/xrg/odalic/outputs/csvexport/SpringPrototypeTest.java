765a9fb5485c12f4e9a1149c24641ffe47404e69
josef-janousek <pauto@centrum.cz>
2017-05-14 20:05:49 +0200

    corrections

--- odalic/src/test/java/cz/cuni/mff/xrg/odalic/outputs/csvexport/SpringPrototypeTest.java
+++ odalic/src/test/java/cz/cuni/mff/xrg/odalic/outputs/csvexport/SpringPrototypeTest.java
@@ -2,16 +2,12 @@ package cz.cuni.mff.xrg.odalic.outputs.csvexport;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.ClassRule;
 import org.junit.Ignore;
-import org.junit.Rule;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.rules.SpringClassRule;
-import org.springframework.test.context.junit4.rules.SpringMethodRule;
 
 import cz.cuni.mff.xrg.odalic.bases.proxies.KnowledgeBaseProxiesService;
 
@@ -25,12 +21,6 @@ import cz.cuni.mff.xrg.odalic.bases.proxies.KnowledgeBaseProxiesService;
 public class SpringPrototypeTest {
 
   private static final Logger log = LoggerFactory.getLogger(SpringPrototypeTest.class);
-
-  @ClassRule
-  public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
-
-  @Rule
-  public final SpringMethodRule springMethodRule = new SpringMethodRule();
 
   @Autowired
   private KnowledgeBaseProxiesService knowledgeBaseProxiesService;
