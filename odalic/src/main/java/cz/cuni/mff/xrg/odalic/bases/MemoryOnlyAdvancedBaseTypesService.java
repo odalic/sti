6271079ba58bf4ddc8c6e9b598583fe5bf59910f
Tomas Knap <toknap@gmail.com>
2017-07-18 14:59:49 +0200

    Towards advanced type in odalic backend

--- odalic/src/main/java/cz/cuni/mff/xrg/odalic/bases/MemoryOnlyAdvancedBaseTypesService.java
+++ odalic/src/main/java/cz/cuni/mff/xrg/odalic/bases/MemoryOnlyAdvancedBaseTypesService.java
@@ -3,18 +3,18 @@
  */
 package cz.cuni.mff.xrg.odalic.bases;
 
-import java.util.Map;
-import java.util.SortedSet;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ImmutableSortedSet;
-
+import cz.cuni.mff.xrg.odalic.bases.types.PPKnowledgeBaseDefinitionFactory;
 import cz.cuni.mff.xrg.odalic.bases.types.SparqlKnowledgeBaseDefinitionFactory;
 import cz.cuni.mff.xrg.odalic.groups.GroupsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import uk.ac.shef.dcs.kbproxy.ProxyDefinition;
+
+import java.util.Map;
+import java.util.SortedSet;
 
 /**
  * Default {@link AdvancedBaseTypesService} implementation.
@@ -23,9 +23,11 @@ import uk.ac.shef.dcs.kbproxy.ProxyDefinition;
 public final class MemoryOnlyAdvancedBaseTypesService implements AdvancedBaseTypesService {
 
   public static final String SPARQL_BASE_TYPE_NAME = "SPARQL";
-
   public static final AdvancedBaseType SPARQL_BASE_TYPE = new AdvancedBaseType(SPARQL_BASE_TYPE_NAME, ImmutableSet.of(), ImmutableMap.of(), ImmutableMap.of());
-  
+
+  public static final String PP_BASE_TYPE_NAME = "PoolParty";
+  public static final AdvancedBaseType PP_BASE_TYPE = new AdvancedBaseType(PP_BASE_TYPE_NAME, ImmutableSet.of("ppServerUrl", "ppPassword"), ImmutableMap.of(), ImmutableMap.of());
+
   private final GroupsService groupsService;
   
   private final Map<? extends String, ? extends AdvancedBaseType> types;
@@ -35,7 +37,7 @@ public final class MemoryOnlyAdvancedBaseTypesService implements AdvancedBaseTyp
 
   @Autowired
   public MemoryOnlyAdvancedBaseTypesService(final GroupsService groupsService) {
-    this(groupsService, ImmutableMap.of(SPARQL_BASE_TYPE_NAME, SPARQL_BASE_TYPE), ImmutableMap.of(SPARQL_BASE_TYPE, new SparqlKnowledgeBaseDefinitionFactory()));
+    this(groupsService, ImmutableMap.of(SPARQL_BASE_TYPE_NAME, SPARQL_BASE_TYPE,PP_BASE_TYPE_NAME, PP_BASE_TYPE), ImmutableMap.of(SPARQL_BASE_TYPE, new SparqlKnowledgeBaseDefinitionFactory(), PP_BASE_TYPE, new PPKnowledgeBaseDefinitionFactory()));
   }
   
   private MemoryOnlyAdvancedBaseTypesService(final GroupsService groupsService, final Map<? extends String, ? extends AdvancedBaseType> types, final Map<? extends AdvancedBaseType, ? extends ProxyDefinitionFactory> typesToDefinitionFactories) {
---
+++ odalic/src/main/java/cz/cuni/mff/xrg/odalic/bases/types/PPKnowledgeBaseDefinitionFactory.java
@@ -0,0 +1,101 @@
+package cz.cuni.mff.xrg.odalic.bases.types;
+
+import com.google.common.collect.ImmutableSet;
+import cz.cuni.mff.xrg.odalic.bases.KnowledgeBase;
+import cz.cuni.mff.xrg.odalic.bases.ProxyDefinitionFactory;
+import cz.cuni.mff.xrg.odalic.bases.TextSearchingMethod;
+import cz.cuni.mff.xrg.odalic.groups.Group;
+import org.springframework.stereotype.Component;
+import uk.ac.shef.dcs.kbproxy.sparql.SparqlProxyDefinition;
+import uk.ac.shef.dcs.kbproxy.sparql.pp.PPProxyDefinition;
+
+import java.util.Set;
+
+@Component
+public final class PPKnowledgeBaseDefinitionFactory
+    implements ProxyDefinitionFactory {
+
+  @Override
+  public PPProxyDefinition create(final KnowledgeBase base, final Set<? extends Group> availableGroups) {
+    final PPProxyDefinition.Builder builder = PPProxyDefinition.builder();
+    
+    // Name
+    builder.setName(base.getQualifiedName());
+
+    // Insert
+    builder.setInsertSupported(base.isInsertEnabled());
+    
+    if (base.isInsertEnabled()) {
+//      if (base.getInsertEndpoint() == null) {
+//        builder.setInsertEndpoint(base.getEndpoint().toString());
+//      } else {
+//        builder.setInsertEndpoint(base.getInsertEndpoint().toString());
+//      }
+      
+      builder.setInsertPrefixData(base.getUserResourcesPrefix());
+      builder.setInsertPrefixSchema(base.getUserClassesPrefix());
+    }
+    
+    // Endpoint
+    builder.setEndpoint(base.getEndpoint().toString());
+    builder.setLogin(base.getLogin());
+    builder.setPassword(base.getPassword());
+
+    // Fulltext settings
+    builder.setFulltextEnabled(base.getTextSearchingMethod() == TextSearchingMethod.SUBSTRING || base.getTextSearchingMethod() == TextSearchingMethod.FULLTEXT);
+    builder.setUseBifContains(base.getTextSearchingMethod() == TextSearchingMethod.FULLTEXT);
+
+    // Language suffix
+    builder.setLanguageSuffix(base.getLanguageTag());
+
+    // Use default class type mode
+    builder.setClassTypeMode(PPProxyDefinition.SEARCH_CLASS_TYPE_MODE_VALUE.DIRECT);
+
+    // Loading structure
+    builder.setGroupsAutoSelected(base.getGroupsAutoSelected());
+    final Set<? extends Group> usedGroups = getUsedGroups(base, availableGroups);
+    for (final Group group : usedGroups) {
+      builder.addAllStructurePredicateLabel(group.getLabelPredicates());
+      builder.addAllStructurePredicateDescription(group.getDescriptionPredicates());
+      builder.addAllStructureTypeClass(group.getClassTypes());
+      builder.addAllStructureTypeProperty(group.getPropertyTypes());
+      builder.addAllStructurePredicateType(group.getInstanceOfPredicates());
+    }
+    
+    builder.setStructureInstanceOf(SparqlProxyDefinition.DEFAULT_STRUCTURE_PREDICATE_INSTANCE_OF);
+    builder.setStructureDomain(SparqlProxyDefinition.DEFAULT_STRUCTURE_PREDICATE_DOMAIN);
+    builder.setStructureRange(SparqlProxyDefinition.DEFAULT_STRUCTURE_PREDICATE_RANGE);
+
+    // SPARQL insert
+    if (base.isInsertEnabled()) {
+//      builder.setInsertGraph(base.getInsertGraph() == null
+//          ? SparqlProxyDefinition.DEFAULT_INSERT_GRAPH : base.getInsertGraph().toString());
+
+      builder.setInsertDefaultClass(SparqlProxyDefinition.DEFAULT_INSERT_DEFAULT_CLASS);
+      builder.setInsertPredicateLabel(SparqlProxyDefinition.DEFAULT_INSERT_PREDICATE_LABEL);
+      builder.setInsertPredicateAlternativeLabel(SparqlProxyDefinition.DEFAULT_INSERT_PREDICATE_ALTERNATIVE_LABEL);
+      builder.setInsertPredicateSubclassOf(SparqlProxyDefinition.DEFAULT_INSERT_PREDICATE_SUBCLASS_OF);
+      builder.setInsertPredicateSubPropertyOf(SparqlProxyDefinition.DEFAULT_INSERT_PREDICATE_SUB_PROPERTY_OF);
+      builder.setInsertTypeClass(SparqlProxyDefinition.DEFAULT_INSERT_TYPE_CLASS);
+      builder.setInsertTypeDataProperty(base.getDatatypeProperty() == null ? SparqlProxyDefinition.DEFAULT_INSERT_DATATYPE_PROPERTY_TYPE : base.getDatatypeProperty().toString());
+      builder.setInsertTypeObjectProperty(base.getObjectProperty() == null ? SparqlProxyDefinition.DEFAULT_INSERT_OBJECT_PROPERTY_TYPE : base.getObjectProperty().toString());
+    }
+    
+    builder.setStoppedClasses(base.getSkippedClasses());
+    builder.setStoppedAttributes(base.getSkippedAttributes());
+    
+    // Apply URI heuristics
+    builder.setUriLabelHeuristicApplied(true);
+      
+    return builder.build();
+  }
+
+  private Set<Group> getUsedGroups(final KnowledgeBase base, final Set<? extends Group> availableGroups) {
+    if (base.getGroupsAutoSelected()) {
+
+      return ImmutableSet.copyOf(availableGroups); // Will be filtered later.
+    } else {
+      return base.getSelectedGroups(); // If auto-detection turned off, just use the ones manually set.
+    }
+  }
+}
